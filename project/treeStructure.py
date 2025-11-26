ec5caa0e98adb17cfd781424ff5604931b05bed5
ladt4210 <tc.ladeira@uleth.ca>
2025-11-26 15:41:54 -0800

    Creating Node of TreeStructure

--- project/treeStructure.py
+++ project/treeStructure.py
@@ -1,3 +1,18 @@
 import random
 import matplotlib.pyplot as plt
-     
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+
+    def add_child(self, child_node):
+        self.children.append(child_node)
+
+    def is_terminal(self):
+        return len(self.children) == 0
+
+    def __repr__(self):
+        return f"Node({self.name})"
+
+
