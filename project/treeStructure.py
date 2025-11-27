8092cfdd4de8df1ab8b6966beccba4578a98e963
ladt4210 <tc.ladeira@uleth.ca>
2025-11-26 16:27:10 -0800

    New format for Node __str__

--- project/treeStructure.py
+++ project/treeStructure.py
@@ -17,7 +17,7 @@ class Node:
     def __str__(self):
         if self.is_terminal():
             return str(self.value)
-        return f"{self.value} -> [{', '.join(str(child) for child in self.children)}]"
+        return f"{self.value} ({', '.join(str(child) for child in self.children)})"
 
 
 #test cases
@@ -27,7 +27,8 @@ n1.add_child(n2)
 print (n1.value)
 print (n1.children[0].value)
 
-print (n1.is_terminal())
-print (n2.is_terminal())
+print(n1.is_terminal())
+print(n2.is_terminal())
 
-print (n1)
+print(n1)
+print(n2)
